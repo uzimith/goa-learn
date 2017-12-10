908c684008a6d8cf073f6122de8bab169b05fdbf
uzimith <uzimith.x9@gmail.com>
2017-12-11 01:23:12 +0900

    move

--- doc/deploy.sh
+++
@@ -1,2 +0,0 @@
-java -jar ./doc/swagger2markup-cli-1.3.1.jar convert -i ./swagger/swagger.json -f doc/api -c ./doc/config.properties
-java -jar ./doc/swagger2markup-cli-1.3.1.jar convert -i ./swagger/swagger.json -f doc/api
